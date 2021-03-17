71abf8a535d54bffad3979f72b3b193147229aa9
pulga23 <79906638+pulga23@users.noreply.github.com>
2021-03-17 14:57:02 +0000

    Initial commit

---
+++ Assets/scripts/Paddle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+   [SerializeField]
+    float velocidade = 0.5f;
+
+    [SerializeField]
+    float AlturaPaddle=1;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+       //movimento paddle
+        if(Input.GetKey(upKey)) 
+        {
+            //Sobe
+            transform.position += velocidade * Vector3.up * Time.deltaTime; //  igual a transform.position = transform.position + velocidade * Vector3.up;
+            
+        }
+       
+        else if (Input.GetKey(downKey)) 
+        {
+            //Desce
+            transform.position += velocidade * Vector3.down *Time.deltaTime;
+        }
+
+       
+        float AlturaCamara = Camera.main.orthographicSize;
+        
+        // Verificar Limites com ifs 
+         if (transform.position.y >= AlturaCamara - (AlturaPaddle*0.5f))
+        {
+             Vector3 posicaoAux = transform.position; //variavel auxiliar pq unity nao deixa alterar y no transform.postion 
+             posicaoAux.y = AlturaCamara - (AlturaPaddle * 0.5f);
+             transform.position = posicaoAux;
+        }
+
+        if (transform.position.y <= -AlturaCamara +0.5f)
+        {
+             Vector3 posicaoAux = transform.position; 
+             posicaoAux.y = -AlturaCamara + (AlturaPaddle * 0.5f);
+             transform.position = posicaoAux;
+        }
+         
+
+        //Verificar Limites com clamps
+        //Vector3 posicaoAux = transform.position;
+        //posicaoAux.y = Mathf.Clamp(posicaoAux.y, - AlturaCamara + (AlturaPaddle*0.5), AlturaCamara - (AlturaPaddle*0.5));
+        //transform.position = posicaoAux;
+
+
+
+
+    }
+}
---
+++ Assets/scripts/ball.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+
+    bool bolaLancada = false;
+    float tempo = 0f;
+
+    float larguracamera;
+
+    int pontosesquerda = 0; 
+    int pontosdireita = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  if (Random.value < 0.5f)
+        //{
+          //  GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
+        //}
+        //else 
+       // {
+         //   GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.left;
+        //}
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        larguracamera = Camera.main.orthographicSize * Camera.main.aspect; //altura da cam x ratio 
+        
+       if (bolaLancada == false)
+       {
+            tempo += Time.deltaTime;
+            if(tempo >= 2f)
+            {
+                bolaLancada = true;
+                Lancarbola();
+            } 
+       }
+
+       //recomeçar quanto toca nas paredes laterais
+       if (transform.position.x >= larguracamera)
+       {
+            pontosesquerda += 1;
+            Lancarbola();
+       }
+       else if (transform.position.x <= -larguracamera)
+       {
+            pontosdireita += 1;
+            Lancarbola();
+       }
+
+        
+
+    }
+    //Laçamento da bola
+    void Lancarbola()
+    {
+        transform.position = 0f * Vector3.forward;
+        GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitCircle;
+        Debug.Log("A pontuação é:");
+        Debug.Log("Jodador 1- " + pontosesquerda + "-" + pontosdireita + " -Jogador 2");
+    }
+}
