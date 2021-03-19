64adc01605600e5c30bb60d8b19edbb368eb6e57
pulga23 <79906638+pulga23@users.noreply.github.com>
2021-03-19 21:08:59 +0000

    funcoes comentadas eliminadas

--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -8,83 +8,49 @@ public class ball : MonoBehaviour
     float velocidade = 5f;
 
     bool bolaLancada = false;
-    float tempoinicial = 0f;
-    float temporelancar = 0f;
     float tempo = 0f;
 
     float larguracamera;
 
     int pontosesquerda = 0; 
     int pontosdireita = 0;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-      //  if (Random.value < 0.5f)
-        //{
-          //  GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
-        //}
-        //else 
-       // {
-         //   GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.left;
-        //}
-        
-    }
-    
+     
 
     // Update is called once per frame
     void Update()
     {
         larguracamera = Camera.main.orthographicSize * Camera.main.aspect; //altura da cam x ratio 
-        
+       
+        // 2 segundos de espera até lançar a bola 
        if (bolaLancada == false)
        {
-            GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.zero;
-            transform.position = 0f * Vector3.forward;
+            GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.zero; //velocidade da bola posta a zero
+            transform.position = 0f * Vector3.forward; //bola volta para posição incial
             tempo += Time.deltaTime;
             if(tempo>= 2f)
             {
                 bolaLancada = true;
                 GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitCircle; //bola lançada com uma direção aleatoria 
-                Debug.Log("A pontuação é:");
-                Debug.Log("Jodador 1- " + pontosesquerda + "-" + pontosdireita + " -Jogador 2");
                 tempo = 0f;
 
             } 
        }
 
        //recomeçar quanto toca nas paredes laterais
-       if (transform.position.x >= larguracamera)
+       if (transform.position.x >= larguracamera) //bola toca parede da direita 
        {
             pontosesquerda += 1;
-            bolaLancada = false;
-       }
-       else if (transform.position.x <= -larguracamera)
-       {
-            pontosdireita += 1;
-            bolaLancada = false;
-       }
-
-        
-
-    }
-    //Laçamento da bola
-    /*void Lancarbola()
-    {
-        GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.zero;
-        transform.position = 0f * Vector3.forward; //bola volta a posição inicial
-      
-        
-            temporelancar += Time.deltaTime;
-        
-      
-        
-            transform.position = 0f * Vector3.forward; //bola volta a posição inicial
-            GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitCircle; //bola lançada com uma direção aleatoria 
             Debug.Log("A pontuação é:");
             Debug.Log("Jodador 1- " + pontosesquerda + "-" + pontosdireita + " -Jogador 2");
-            temporelancar = 0f;
-        
-        
-    }*/
+            bolaLancada = false;
+       }
+       else if (transform.position.x <= -larguracamera) //bola toca parede da esquerda
+       {
+            pontosdireita += 1;
+            Debug.Log("A pontuação é:");
+            Debug.Log("Jodador 1- " + pontosesquerda + "-" + pontosdireita + " -Jogador 2");
+            bolaLancada = false;
+       }
+
+    }
 }
